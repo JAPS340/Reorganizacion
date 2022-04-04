e8377b6af550f5d3095b95da08fcfa6af90626b9
JAPS340 <100705603+JAPS340@users.noreply.github.com>
2022-04-04 12:43:04 -0600

    C13

--- app/mysql.php
+++ app/mysql.php
@@ -1,3 +1,13 @@
 <?php
-   //TODO: CONEXION DE MYSQL
+    use \mysql;
+
+    class Mysql extends \Mysql {
+        protected $data = [
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 'UAdeO',
+            'port' => 3306
+        ];
+    }
 ?>
